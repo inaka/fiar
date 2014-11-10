bc4577900b22c87bad074fec37248d5d297cc574
Euen <euenlopez@gmail.com>
2014-11-10 15:25:12 -0300

    [#11] Refactored fiar.js.

--- priv/static/js/fiar/fiar.js
+++ priv/static/js/fiar/fiar.js
@@ -1,8 +1,6 @@
 Broadcast = {
-  busy_players : [],
-  connect : function(){
+  connect : function() {
     var es = new EventSource('/events');
-
 
     es.onerror = function(e){
       delete_cookie("auth");
@@ -14,66 +12,62 @@ Broadcast = {
       $('#modal_login').foundation('reveal', 'close');
       cleanLoginForm();
     }
-    es.addEventListener('users_connected', function(e) {
-      var msg = $.parseJSON(e.data);
-      if (msg.length > 0) {
-        fillBusyList(msg);
-        updatePlayers(msg);
-        if(isBusy(Broadcast.current_user))
-          Board.enable();
-      };
-    }, false);
-    es.addEventListener('user_connected', function(e) {
-      Broadcast.msg = $.parseJSON(e.data);
-      if (Broadcast.current_user.user.username != Broadcast.msg.user.username) {
-        updateBusyPlayer(Broadcast.msg);
-        updatePlayers(Broadcast.msg);
-      };
-    }, false);
-    es.addEventListener('user_disconnected', function(e) {
-      Broadcast.msg = $.parseJSON(e.data);
-    }, false);
-    es.addEventListener('match_started', function(e) {
-      var match = $.parseJSON(e.data);
-      console.log("match_started BR");
-      console.log(match);
-      updateCurrentUser(match);
-      updateBusyPlayerById(match.player1);
-      updateBusyPlayerById(match.player2);
-      setAsBusy(Broadcast.busy_players);
-      if (Broadcast.current_user.user.id == match.player2) {
-        openInvitationModal();
-      };
-    }, false);
-    es.addEventListener('match_ended', function(e) {
-      var match = $.parseJSON(e.data);
-      console.log("match_ended BR");
-      console.log(match);
-      updateCurrentUser(match);
-      updateBusyPlayerById(match.player1); 
-      updateBusyPlayerById(match.player2);
-      setAsFree([match.player1, match.player2]);
-      Board.disable();
-    }, false);
-    getCurrentUser();
+    es.addEventListener('users_connected', Broadcast.usersConnected, false);
+    es.addEventListener('user_connected', Broadcast.userConnected, false);
+    es.addEventListener('user_disconnected', Broadcast.userDisconnected, false);
+    es.addEventListener('match_started', Broadcast.matchStarted, false);
+    es.addEventListener('match_ended', Broadcast.matchEnded, false);
+
+    Players.loadCurrent();
+  },
+  usersConnected : function(e) {
+    var users = $.parseJSON(e.data);
+    if (users.length > 0) {
+      Players.init(users);
+
+      Players.updateView();
+      Board.updateView();
+    };
+  },
+  userConnected : function(e) {
+    var player = $.parseJSON(e.data);
+    Players.add(player);
+
+    Players.updateView();
+  },
+  userDisconnected : function(e) {
+    Broadcast.msg = $.parseJSON(e.data);
+    Players.updateView();
+  },
+  matchStarted : function(e) {
+    var match = $.parseJSON(e.data);
+    console.log("match_started BR");
+    console.log(match);
+
+    Players.updateCurrent(match, "start");
+    
+    Players.updateStatus(match.player1, match);
+    Players.updateStatus(match.player2, match);
+
+    Players.updateView();
+    Board.updateView();
+
+    Match.showInvitation(Players.current.user.id, match);
+  },
+  matchEnded : function(e) {
+    var match = $.parseJSON(e.data);
+    console.log("match_ended BR");
+    console.log(match);
+
+    Players.updateCurrent(match, "end");
+
+    Players.updateStatus(match.player1, undefined); 
+    Players.updateStatus(match.player2, undefined);
+
+    Players.updateView();
+    Board.updateView();
   }
 };
-
-/**** Invitation ****/
-function openInvitationModal() {
-  $('#modal_invitation').foundation('reveal', 'open');
-}
-
-function closeInvitationModal() {
-  $('#modal_invitation').foundation('reveal', 'close');
-}
-
-function invitationAccepted(){
-  var lastMatchPos = Broadcast.current_user.current_matches.length - 1;
-  var match = Broadcast.current_user.current_matches[lastMatchPos];
-  Match.connect(match);
-  Board.setFirstTurn(match.player2);  
-}
 
 /*** Turn ***/
 Board = {
@@ -89,36 +83,37 @@ Board = {
     $("#board_notice").html("");
     $("#on_hold").show();
   },
-  setFirstTurn : function(playerId) {
-    if (playerId == Broadcast.current_user.user.id) {
-      $("#board_notice").html("<p>Match started, please select a column and play!</p>");
-    };
+  setFirstTurn : function() {
+    $("#board_notice")
+      .html("<p>Match started, please select a column and play!</p>");
   },
   setTurn : function() {
     $("#board_notice").html("<p>Turn.</p>");
+  },
+  updateView : function() {
+    if(Players.isBusy(Players.current))
+      Board.enable();
+    else
+      Board.disable();
   }
 };
 
 /* match connection */
 Match = {
   start : function(event){
-    if (Broadcast.current_user.user.username != event.target.innerText) {
-      var username = event.target.innerText;
-      var url = "/matches";
-      var method = "POST";
-      var data = JSON.stringify({'player2': username});
-      sendRequest(url, method, data, Match.connect);
-    };
+    var username = event.target.innerText;
+    var url = "/matches";
+    var method = "POST";
+    var data = JSON.stringify({'player2': username});
+    sendRequest(url, method, data, Match.connect);
   },
   end : function(){
-    if (Broadcast.current_user.current_matches != undefined) {
-      Broadcast.current_user.current_matches.forEach(function (match) {
-        var url = "/matches/"+match.id;
-        var method = "DELETE";
-        var data = "";
-        sendRequest(url, method, data);
-      });
-    };
+    Players.current.current_matches.forEach(function (match) {
+      var url = "/matches/"+match.id;
+      var method = "DELETE";
+      var data = "";
+      sendRequest(url, method, data);
+    });
   },
   connect : function(data) {
     var es = new EventSource("/matches/" + data.id + "/events");
@@ -134,149 +129,108 @@ Match = {
       console.log("match_ended ME");
       console.log(Match.match);
     }, false);
+  },
+  showInvitation : function(playerId, match) {
+    if (playerId == match.player2) {
+      $('#modal_invitation').foundation('reveal', 'open');
+    }
+  },
+  rejectInvitation : function() {
+    Match.end();
+    $('#modal_invitation').foundation('reveal', 'close');
+  },
+  acceptInvitation : function() {
+    if(Players.isBusy(Players.current)) {
+      var match = Utils.last(Players.current.current_matches);
+      Match.connect(match);
+      Board.setFirstTurn(match.player2);
+    }
+    $('#modal_invitation').foundation('reveal', 'close');
+  }
+}
+
+Players = {
+  connected : [],
+  current : undefined,
+  init : function(users) {
+    users.forEach(Players.add);
+  },
+  add : function(player) {
+    var playerExists = Players._findById(player.user.id);
+    if(player.user.id != Players.current.user.id && !playerExists)
+      Players.connected.push(player);
+  },
+  updateStatus : function(playerId, match) {
+    var player = Players._findById(playerId);
+    if(player)
+        player.current_matches = match ? [match] : [];
+  },
+  loadCurrent : function() {
+    var url = "/me";
+    var method = "GET";
+    var data = "";
+    var success = function(data) {
+      Players.current = data;
+      Players.current.busy = data.current_matches.length > 0;
+    };
+    sendRequest(url, method, data, success);
+  },
+  updateCurrent :function(match, event) {
+    var playerId = Players.current.user.id;
+    if (playerId == match.player1 || playerId == match.player2) {
+      if(event == "start") 
+        Players.current.current_matches = [match];
+      else if(event == "end")
+        Players.current.current_matches = [];
+    }
+  },
+  isBusy : function(player) {
+   return player.current_matches.length > 0;
+  },
+  updateView :function() {
+    $("ul#players_online").empty();
+    Players.connected.forEach(Players._updateViewPlayer);
+  },
+  _updateViewPlayer : function(player) {
+    var li = $("<li></li>")
+               .addClass('player')
+               .attr('id', 'player_' + player.user.id)
+               .text(player.user.username);
+    var busy = Players.isBusy(player);
+    if(busy)
+      li.addClass("busy");
+    else
+      li.addClass("available");
+
+    $("ul#players_online").append(li);
+  },
+  _findById : function(id) {
+    var found = undefined;
+    Players.connected.forEach(function(player) {
+      if(player.user.id == id) found = player;
+    });
+    return found;
+  }
+}
+
+Utils = {
+  last : function(array) {
+    return array[array.length - 1];
   }
 }
 
 /*** On load ***/
-$( document ).ready(function() {
+$(document).ready(function() {
   if (isCookie("auth")) {
     Broadcast.connect();
   };
   $("body").on('click', '#end_match_btn', Match.end);
-  $("body").on('click', '.player a', Match.start);
-  $("body").on('click', '#accept_btn', function () {
-    invitationAccepted();
-    closeInvitationModal();
-  });
-
-  $("body").on('click', '#decline_btn', function () {
-    Match.end();
-    closeInvitationModal();
-  });
-
-  $('#modal_invitation .close-reveal-modal').click(invitationAccepted);
-
-  $("body").on('click', '.reveal-modal-bg', invitationAccepted);
+  $("body").on('click', 'li.player.available', Match.start);
+  $("body").on('click', '#accept_btn', Match.acceptInvitation);
+  $("body").on('click', '#decline_btn', Match.rejectInvitation);
+  $('#modal_invitation .close-reveal-modal').click(Match.acceptInvitation);
+  $("body").on('click', '.reveal-modal-bg', Match.acceptInvitation);
 });
-
-/*** Current user ***/
-function getCurrentUser(){
-  var url = "/me";
-  var method = "GET";
-  var data = "";
-  sendRequest(url, method, data, setCurrentUser);
-};
-
-function setCurrentUser(data){
-  Broadcast.current_user = data;
-};
-
-function updateCurrentUser(match){
-  if (Broadcast.current_user.user.id == match.player1 ||
-      Broadcast.current_user.user.id == match.player2) {
-    if (Broadcast.current_user.current_matches == undefined ||
-        Broadcast.current_user.current_matches.length < 1) {
-      Broadcast.current_user.current_matches = [match];
-    }else{
-      delete Broadcast.current_user.current_matches;
-    }
-  };
-}
-
-/*** Busy list ***/
-function fillBusyList(players){
-  players.forEach(function (player) {
-    updateBusyPlayer(player); 
-  });
-}
-
-function updateBusyPlayerById(playerid){
-  if (isBusy(playerId)) {
-    var index = Broadcast.busy_players.indexOf(playerId);
-    Broadcast.busy_players.splice(index, 1);
-  }else{  
-    Broadcast.busy_players.push(playerId);
-  }
-};
-
-function updateBusyPlayer(player){
-  playerId = player.user.id;
-  if (player.current_matches == undefined || player.current_matches.length > 0) {
-    if (!isBusy(playerId)) {
-      Broadcast.busy_players.push(playerId);
-    };
-  }else{
-    var index = Broadcast.busy_players.indexOf(playerId);
-    if (index != -1) {
-      Broadcast.busy_players.splice(index, 1);
-    }else{
-      console.log("busy player not added to list");
-    };
-  }
-};
-
-function isBusy(playerId){
-  jQuery.inArray(playerId, Broadcast.busy_players) > -1;
-};
-
-function setAsBusy(ids) {
-  ids.forEach(function (id){
-    var username = $('#player_'+id+' a').text();
-    $('#player_'+id).html( "<li id='player_"
-                         + id
-                         + "' class='player busy'>"
-                         + username
-                         + "</li>");
-  });
-};
-function setAsFree(ids) {
-  ids.forEach(function (id){
-    var username = $('#player_'+id).text();
-    $('#player_'+id).html( "<li id='player_"
-                         + id
-                         + "' class='player'><a href='#'>"
-                         + username
-                         + "</a></li>");
-  });
-};
-
-/*** Update list ***/
-function updatePlayers(msg) {
-  console.log(msg);
-  if (msg.length > 0) {
-    $("ul#players_online").html("");
-    Object.keys(msg).forEach(function (key) {
-      if (msg[key].current_matches != undefined) {
-        if (msg[key].current_matches.length > 0) {
-          updatePlayer(msg[key].user, true);
-        } else {
-          updatePlayer(msg[key].user, isBusy(msg.id));
-        }
-      }
-    });
-  }else{
-    $("#player_"+msg.user.id).remove();
-    updatePlayer(msg.user, isBusy(msg.user.id));
-  }
-};
-
-function updatePlayer(msg, match){
-  if (match) {
-    $("ul#players_online").append( "<li id='player_"
-                                 + msg.id
-                                 + "' class='player busy'>"
-                                 + msg.username
-                                 + "</li>"
-                                 );
-  }else{
-    $("ul#players_online").append( "<li id='player_"
-                                 + msg.id 
-                                 + "' class='player'><a href='#'>"
-                                 + msg.username
-                                 + "</a></li>"
-                                 );
-  };
-};
 
 /*** ajax request ***/
 function sendRequest(url, method, data2, success) {
